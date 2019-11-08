03113e78c7f4b366b59e8496748e8b99379931a2
Yuliaaaaaa <jshcherbakova@gmail.com>
2019-11-08 02:57:02 +0200

    Changed connection configurations

--- src/main/java/parsers/Parser.java
+++ src/main/java/parsers/Parser.java
@@ -21,6 +21,8 @@ public abstract class Parser {
 
     protected Connection connect() {
         return Jsoup.connect(url)
+                .header("Accept-Encoding", "gzip, deflate")
+                .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0")
                 .maxBodySize(0)
                 .timeout(6000)
                 .ignoreContentType(true);
